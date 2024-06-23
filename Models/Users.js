52a0bd0e37c816142c44e3814b75c05f520d93b6
Finser K <koriatyehonatan@gmail.com>
2024-06-23 18:24:49 +0300

    some name conventions

---
+++ Models/Users.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    u_id: { type: Number, required: true },
+    u_username: { type: String, required: true },
+    u_email: { type: String, required: true },
+    u_birthdate: { type: Date, required: true },
+    u_password: { type: String, required: true },
+    u_admin: { type: Boolean, required: true }
+})
+
+module.exports = mongoose.model("User", User);
