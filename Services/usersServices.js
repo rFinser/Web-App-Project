a5a916c7e091a20c9f9b0e4928eb08ecdeea55ae
Finser K <koriatyehonatan@gmail.com>
2024-06-23 21:49:26 +0300

    removed usersModel.js

--- Services/usersServices.js
+++ Services/usersServices.js
@@ -1,22 +1,22 @@
-const Users = require('./Models/usersModel')//change to ../Models/usersModel
+const Users = require('../Models/usersModel')//change to ../Models/usersModel
 
-async function login(id){
-    const user =findUser(id)
+async function login(id) {
+    const user = findUser(id)
     //user not exsist, need to register
-    if(user==null){
+    if (user == null) {
         //TODO
     }
     return user
 }
 
-async function register(id,username,email,password,birthdate,admin){
+async function register(id, username, email, password, birthdate, admin) {
     new user = new Users({
-        id,username,email,birthdate,password,admin
+        id, username, email, birthdate, password, admin
     })
     await user.save()
 }
 
-async function findUser(id){
-    const user = await Users.findOne({id:id});
+async function findUser(id) {
+    const user = await Users.findOne({ id: id });
     return user
 }
