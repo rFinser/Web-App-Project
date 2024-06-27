ea56977b7a9c594c4993c51f3388b5eca4049f0b
rFinser <koriatyehonatan@gmail.com>
2024-06-28 00:25:22 +0300

    some bug fixes

--- Services/productsServices.js
+++ Services/productsServices.js
@@ -1,7 +1,5 @@
 const Product = require("../Models/Products");
-/***
- * Creates a Product with the given params, unless a product with the given id already exists, in which case it throws an error
- */
+
 async function createProduct(id, name, price, desc, tags, restaurantName) {
     //id is key
     if (await findProductById(id) != null)
@@ -10,18 +8,11 @@ async function createProduct(id, name, price, desc, tags, restaurantName) {
     await product.save();
 }
 
-/***
- * @param {Number} id id of the product to find 
- * @returns the product doc or null incase it doesnt exist
- */
 async function findProductById(id) {
-    const product = await Product.findOne({ id });
+    const product = await Product.findOne({ p_id: id });
     return product; //either null or the actual product, make sure to check
 }
 
-/***
- * @returns All of the products as a list
- */
 async function getAllProducts() {
     return await Product.find();
 }
@@ -29,23 +20,31 @@ async function getAllProducts() {
 async function updateProduct(id_to_update, name, price, desc, tags, restaurantName) {
     if (await findProductById(id_to_update) == null)
         throw Error(`No product for ${id_to_update} was found.`);
-    await Product.updateOne({ id: id_to_update }, { id: id_to_update, p_name: name, p_price: price, p_description: desc, p_tags: tags, p_restaurantName: restaurantName });
+    await Product.updateOne({ p_id: id_to_update }, { p_id: id_to_update, p_name: name, p_price: price, p_description: desc, p_tags: tags, p_restaurantName: restaurantName });
 }
 
 async function deleteProduct(id_to_delete) {
     if (await findProductById(id_to_delete) == null)
         throw Error(`No Product for ${id_to_delete} was found.`);
-    await Product.deleteOne({ id: id_to_delete });
+    await Product.deleteOne({ p_id: id_to_delete });
+}
+
+function isSubArray(array, subArray) {
+    for (let i = 0; i < subArray.length; i++) {
+        if (!array.includes(subArray[i]))
+            return false;
+    }
+    return true;
 }
 
 async function findByTags(tags) {
     let foundProducts = new Set();
     const products = await getAllProducts();
     for (const product of products) {
-        for (const tag of tags) {
-            if (product.p_tags.includes(tag))
-                foundProducts.add(product);
-        }
+        if (product.p_tags == null)
+            continue;
+        if (isSubArray(product.p_tags, tags))
+            foundProducts.add(product);
     }
     return foundProducts;
 }
