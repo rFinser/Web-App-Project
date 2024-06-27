d758ed81e4bbf274edac5fc36a35771f5a2367fc
rFinser <koriatyehonatan@gmail.com>
2024-06-27 21:55:08 +0300

    added searchBy function to the services

--- Services/productsServices.js
+++ Services/productsServices.js
@@ -1,13 +1,12 @@
 const Product = require("../Models/Products");
-
 /***
  * Creates a Product with the given params, unless a product with the given id already exists, in which case it throws an error
  */
-async function createProduct(id, name, price, desc) {
+async function createProduct(id, name, price, desc, tags, restaurantName) {
     //id is key
     if (await findProductById(id) != null)
         throw Error(`A product already exists for ${id}`);
-    let product = new Product({ p_id: id, p_name: name, p_price: price, p_description: desc });
+    let product = new Product({ p_id: id, p_name: name, p_price: price, p_description: desc, p_tags: tags, p_restaurantName: restaurantName });
     await product.save();
 }
 
@@ -27,10 +26,10 @@ async function getAllProducts() {
     return await Product.find();
 }
 
-async function updateProduct(id_to_update, name, price, desc) {
+async function updateProduct(id_to_update, name, price, desc, tags, restaurantName) {
     if (await findProductById(id_to_update) == null)
         throw Error(`No product for ${id_to_update} was found.`);
-    await Product.updateOne({ id: id_to_update }, { id: id_to_update, p_name: name, p_price: price, p_description: desc });
+    await Product.updateOne({ id: id_to_update }, { id: id_to_update, p_name: name, p_price: price, p_description: desc, p_tags: tags, p_restaurantName: restaurantName });
 }
 
 async function deleteProduct(id_to_delete) {
@@ -39,10 +38,29 @@ async function deleteProduct(id_to_delete) {
     await Product.deleteOne({ id: id_to_delete });
 }
 
+async function findByTags(tags) {
+    let foundProducts = new Set();
+    const products = await getAllProducts();
+    for (const product of products) {
+        for (const tag of tags) {
+            if (product.p_tags.includes(tag))
+                foundProducts.add(product);
+        }
+    }
+    return foundProducts;
+}
+
+async function findByPrice(minPrice, maxPrice) {
+    let foundProducts = await Product.find({ p_price: { $gte: minPrice, $lte: maxPrice } });
+    return foundProducts;
+}
+
 module.exports = {
     createProduct,
     findProductById,
     getAllProducts,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    findByTags,
+    findByPrice
 }
