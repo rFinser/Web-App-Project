782acf344209d84f2603f412c258dd9ec5b476e8
rFinser <koriatyehonatan@gmail.com>
2024-06-28 08:43:16 +0300

    restaurant model

---
+++ Models/Restaurant.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Restaurant = new mongoose.Schema({
+    r_name: { type: String, required: true },
+    r_description: { type: String, required: true },
+    r_icon: String, //path to the image
+    r_tags: Array,
+    r_address: { type: String, required: true },
+    r_geolocation: { type: Array, required: true }, //coordinates (longitude, latitude), would be helpful when using maps api
+})
+
+module.exports = mongoose.model("Restaurant", Restaurant);
