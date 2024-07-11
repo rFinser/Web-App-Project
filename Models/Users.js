a1d3ecf918c478339a0c27e08e59797633686045
rFinser <koriatyehonatan@gmail.com>
2024-07-11 18:03:31 +0300

    added a registerationDate field to the users

--- Models/Users.js
+++ Models/Users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Birthdate = new Schema({
+const date = new Schema({
     day: {type: Number, required: true},
     month: {type: Number, required:true},
     year: {type: Number, required:true},
@@ -9,8 +9,9 @@ const Birthdate = new Schema({
 const User = new Schema({
     u_username: { type: String, required: true },
     u_email: { type: String, required: true }, 
-    u_birthdate: {type:Birthdate, required: true},
+    u_birthdate: {type:date, required: true},
     u_password: { type: String, required: true },
+    u_registrationDate: {type: date, required: true},
     u_admin: { type: Boolean, required: true },
     u_cart: {type:Array, default:[]},
 })
